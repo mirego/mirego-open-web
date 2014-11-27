1b3e41011dcc99eabd76deff91fb9d13bb01a101
Simon Prévost <sprevost@mirego.com>
2014-11-27 15:51:14 -0500

    Refactor resource syntax

--- lib/repo/resource.ex
+++ lib/repo/resource.ex
@@ -1,3 +1,10 @@
 defmodule OpenMirego.Repo.Resource do
-  defstruct name: nil, description: nil, language: nil, pretty_language: nil, url: nil, pushed_at: nil, raw_pushed_at: nil
+  defstruct name: nil,
+    description: nil,
+    language: nil,
+    pretty_language: nil,
+    url: nil,
+    pushed_at: nil,
+    raw_pushed_at: nil,
+    fork: nil
 end
