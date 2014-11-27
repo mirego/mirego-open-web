9119faf78719a59216ed7a8731412e2a9aa9a8e7
Simon Prévost <sprevost@mirego.com>
2014-11-27 15:51:45 -0500

    Remove useless comments in supervisor

--- lib/open_mirego.ex
+++ lib/open_mirego.ex
@@ -6,14 +6,9 @@ defmodule OpenMirego do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [
-      # Define workers and child supervisors to be supervised
-      # worker(OpenMirego.Worker, [arg1, arg2, arg3])
-    ]
-
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: OpenMirego.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link([], opts)
   end
 end
