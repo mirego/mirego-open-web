aa6df31eb35c6cf7389757c41a4200145b6d2368
Simon Prévost <sprevost@mirego.com>
2014-12-12 10:39:27 -0500

    Add Resource pattern match to reject repo

--- lib/repo/collection_serializer.ex
+++ lib/repo/collection_serializer.ex
@@ -1,5 +1,6 @@
 defmodule OpenMirego.Repo.CollectionSerializer do
   use Timex
+  alias OpenMirego.Repo.Resource
   alias OpenMirego.Repo.Serializer
 
   def serialize(json) do
@@ -9,7 +10,6 @@ defmodule OpenMirego.Repo.CollectionSerializer do
     |> Enum.sort(&(&1.pushed_at < &2.pushed_at))
   end
 
-  defp reject?(repo) do
-    !repo.visible
-  end
+  defp reject?(%Resource{visible: false}), do: true
+  defp reject?(%Resource{}), do: false
 end
