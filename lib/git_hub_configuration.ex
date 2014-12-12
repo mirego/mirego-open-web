1cf25b7e4cfa1b7d983d496c878c9ebf11f69c4c
Simon Prévost <sprevost@mirego.com>
2014-12-12 10:38:27 -0500

    Fix organization id

--- lib/git_hub_configuration.ex
+++ lib/git_hub_configuration.ex
@@ -1,5 +1,5 @@
 defmodule GitHubConfiguration do
   def headers, do: [{:"User-Agent", "Open-Source @ Mirego (http://open.mirego.com)"}]
-  def organization, do: "1202891" # mirego
+  def organization, do: "mirego"
   def authentication, do: "#{Application.get_env(:github, OpenMirego.Router)[:api_key]}:x-oauth-basic"
 end
