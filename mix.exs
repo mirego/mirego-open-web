e00e1f918c14eba8b7470d6b34132c900c5e72fb
Simon Prévost <sprevost@mirego.com>
2015-05-11 16:25:32 -0400

    Update phoenix 0.13

--- mix.exs
+++ mix.exs
@@ -26,7 +26,8 @@ defmodule OpenMirego.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.11"},
+    [{:phoenix, "~> 0.13"},
+     {:phoenix_html, "~> 1.0"},
      {:con_cache, "~> 0.6.1"},
      {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.1"},
      {:httpotion, "~> 0.2.0"},
