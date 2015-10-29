db55d6cc17a94404bdcc724be799aee717c82f32
Rémi Prévost <remi@exomel.com>
2015-10-29 14:52:27 -0400

    Upgrade to Elixir 1.1

--- lib/git_hub_configuration.ex
+++ lib/git_hub_configuration.ex
@@ -1,5 +1,5 @@
 defmodule GitHubConfiguration do
   def headers, do: [{:"User-Agent", "Open-Source @ Mirego (http://open.mirego.com)"}]
   def organization, do: "mirego"
-  def authentication, do: "#{Application.get_env(:github, OpenMirego.Router)[:api_key]}:x-oauth-basic"
+  def authentication, do: "#{Application.get_env(:open_mirego, OpenMirego.GitHubConfiguration)[:api_key]}:x-oauth-basic"
 end
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -2,6 +2,6 @@ defmodule OpenMirego.LayoutView do
   use OpenMirego.Web, :view
 
   def google_analytics_tracker_id do
-    Application.get_env(:google_analytics, OpenMirego.Router)[:tracker_id]
+    Application.get_env(:open_mirego, OpenMirego.GoogleAnalytics)[:tracker_id]
   end
 end
