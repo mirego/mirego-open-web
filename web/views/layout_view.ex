46173ec961b7b8570b8413cc7b63d4455a47b140
Rémi Prévost <remi@exomel.com>
2016-01-12 08:20:01 -0500

    Use Kernel#get_in/2

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -2,10 +2,10 @@ defmodule OpenMirego.LayoutView do
   use OpenMirego.Web, :view
 
   def google_analytics_tracker_id do
-    Application.get_env(:open_mirego, OpenMirego.GoogleAnalytics)[:tracker_id]
+    get_in(Application.get_env(:open_mirego, OpenMirego.GoogleAnalytics), [:tracker_id])
   end
 
   def typekit_kit_id do
-    Application.get_env(:open_mirego, OpenMirego.Typekit)[:kit_id]
+    get_in(Application.get_env(:open_mirego, OpenMirego.Typekit), [:kit_id])
   end
 end
