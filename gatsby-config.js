7c6e5b099141d13d683dccbd9d37eab9b12d8c57
Rémi Prévost <rprevost@mirego.com>
2020-11-16 08:02:50 -0500

    Remove trailing comma Prettier config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,16 +5,16 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKER_ID
-      }
-    }
-  ]
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKER_ID,
+      },
+    },
+  ],
 };
