360825393ad2ec27b1e1c5cae6e29793985c0974
Rémi Prévost <rprevost@mirego.com>
2021-01-15 09:29:36 -0500

    Reorder projects on homepage

--- data/projects.js
+++ data/projects.js
@@ -32,6 +32,13 @@ module.exports = [
     tags: ['javascript', 'typescript', 'react', 'boilerplate'],
   },
   {
+    slug: 'trikot.patron',
+    name: 'Trikot.patron',
+    logo: 'trikot.svg',
+    description: 'Kotlin Multiplatform Sample Project using Trikot libraries.',
+    tags: ['kotlin', 'kotlin-multiplatform'],
+  },
+  {
     slug: 'dispatch',
     logo: 'dispatch.svg',
     name: 'Dispatch',
@@ -96,50 +103,12 @@ module.exports = [
     tags: ['elixir', 'html', 'exunit'],
   },
   {
-    slug: 'activerecord_json_validator',
-    logo: 'activerecord-json-validator.svg',
-    name: 'ActiveRecord::JSONValidator',
-    description:
-      'ActiveRecord::JSONValidator makes it easy to validate JSON attributes against a JSON schema.',
-    tags: ['ruby', 'rails', 'json-schema'],
-  },
-  {
-    slug: 'microscope',
-    logo: 'microscope.svg',
-    name: 'Microscope',
-    description:
-      'Microscope adds useful scopes targeting ActiveRecord boolean, date and datetime fields.',
-    tags: ['ruby', 'rails'],
-  },
-  {
-    slug: 'gaffe',
-    logo: 'gaffe.svg',
-    name: 'Gaffe',
-    description: 'Gaffe handles Rails error pages in a clean, simple way.',
-    tags: ['ruby', 'rails'],
-  },
-  {
     slug: 'stylelint-mirego',
     logo: 'stylelint-mirego.svg',
     name: 'stylelint-mirego',
     description:
       'A collection of Stylelint rules that help our code meet our code styling guidelines.',
     tags: ['javascript', 'css', 'sass', 'stylelint'],
-  },
-  {
-    slug: 'ember-chai-dom-helpers',
-    logo: 'ember-chai-dom-helpers.svg',
-    name: 'ember-chai-dom-helpers',
-    description: 'A set of Chai helpers ready for your DOM.',
-    tags: ['javascript', 'ember', 'chai'],
-  },
-  {
-    slug: 'trikot.analytics',
-    name: 'Trikot.analytics',
-    logo: 'trikot.svg',
-    description:
-      'Implementation of Multiplatform Analytics in ReactiveStreams.',
-    tags: ['kotlin', 'kotlin-multiplatform', 'analytics', 'reactive-streams'],
   },
   {
     slug: 'trikot.streams',
@@ -165,13 +134,6 @@ module.exports = [
     tags: ['kotlin', 'kotlin-multiplatform'],
   },
   {
-    slug: 'trikot.patron',
-    name: 'Trikot.patron',
-    logo: 'trikot.svg',
-    description: 'Kotlin Multiplatform Sample Project using Trikot libraries.',
-    tags: ['kotlin', 'kotlin-multiplatform'],
-  },
-  {
     slug: 'trikot.http',
     name: 'Trikot.http',
     logo: 'trikot.svg',
@@ -192,6 +154,44 @@ module.exports = [
     logo: 'trikot.svg',
     description: 'Multiplaform cache layers abstraction.',
     tags: ['kotlin', 'kotlin-multiplatform', 'cache'],
+  },
+  {
+    slug: 'trikot.analytics',
+    name: 'Trikot.analytics',
+    logo: 'trikot.svg',
+    description:
+      'Implementation of Multiplatform Analytics in ReactiveStreams.',
+    tags: ['kotlin', 'kotlin-multiplatform', 'analytics', 'reactive-streams'],
+  },
+  {
+    slug: 'activerecord_json_validator',
+    logo: 'activerecord-json-validator.svg',
+    name: 'ActiveRecord::JSONValidator',
+    description:
+      'ActiveRecord::JSONValidator makes it easy to validate JSON attributes against a JSON schema.',
+    tags: ['ruby', 'rails', 'json-schema'],
+  },
+  {
+    slug: 'microscope',
+    logo: 'microscope.svg',
+    name: 'Microscope',
+    description:
+      'Microscope adds useful scopes targeting ActiveRecord boolean, date and datetime fields.',
+    tags: ['ruby', 'rails'],
+  },
+  {
+    slug: 'gaffe',
+    logo: 'gaffe.svg',
+    name: 'Gaffe',
+    description: 'Gaffe handles Rails error pages in a clean, simple way.',
+    tags: ['ruby', 'rails'],
+  },
+  {
+    slug: 'ember-chai-dom-helpers',
+    logo: 'ember-chai-dom-helpers.svg',
+    name: 'ember-chai-dom-helpers',
+    description: 'A set of Chai helpers ready for your DOM.',
+    tags: ['javascript', 'ember', 'chai'],
   },
   {
     slug: 'taylor-ios',
